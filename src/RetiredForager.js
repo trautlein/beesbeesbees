dc3accb811f88c816821f46765fee6cb99294aa8
Hans Trautlein <hans@trautlein.com>
2016-09-19 11:39:08 -0700

    Went from "new" to "Object.create"
    Shoudl have done this earlier. Glad it has been completed now!

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,11 +1,12 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
   this.age = 40;
   this.job = 'gamble';
   this.canFly = false;
   this.color = 'grey';
 };
 
-RetiredForagerBee.prototype  = new ForagerBee();
+RetiredForagerBee.prototype  = Object.create(ForagerBee.prototype);
 RetiredForagerBee.prototype.constructor = RetiredForagerBee;
 
 RetiredForagerBee.prototype.forage = function() {
